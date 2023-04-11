c11063b6ed363dd51647e36939eb9c8908bf5898
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-04-11 19:20:19 +0600

    statistics page completed

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,6 +1,49 @@
-import React from 'react';
+import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 
 const Statistics = () => {
+  const marksArray = [
+    {
+      name: 'Assignment 1',
+      marks: 60
+    },
+    {
+      name: 'Assignment 2',
+      marks: 60
+    },
+    {
+      name: 'Assignment 3',
+      marks: 60
+    },
+    {
+      name: 'Assignment 4',
+      marks: 58
+    },
+    {
+      name: 'Assignment 5',
+      marks: 60
+    },
+    {
+      name: 'Assignment 6',
+      marks: 57
+    },
+    {
+      name: 'Assignment 7',
+      marks: 59
+    },
+    {
+      name: 'Assignment 8',
+      marks: 60
+    }
+  ]
+
   return (
     <div>
       <div className="bg-gradient-to-b from-sky-50 to-blue-50">
@@ -9,6 +52,19 @@ const Statistics = () => {
             Statistics
           </h1>
         </div>
+      </div>
+      <div className="max-w-[1600px] my-32 mx-auto">
+        <AreaChart
+          width={1600}
+          height={400}
+          data={marksArray}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="marks" stroke="blue" fill="skyblue" />
+        </AreaChart>
       </div>
     </div>
   );
