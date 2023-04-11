98bfeaa7d365dc160f4072c571a1423957db35b8
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-04-12 03:07:38 +0600

    added jobCategory.json and used it inside job category list section

--- src/components/JobCategory.jsx
+++ src/components/JobCategory.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalculator, faLightbulb, faChartSimple, faMicrochip } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalculator,
+  faLightbulb,
+  faChartSimple,
+  faMicrochip,
+} from "@fortawesome/free-solid-svg-icons";
 
 const JobCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("jobCategory.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  console.log(categories);
   return (
     <div className="max-w-[1600px] mx-auto mt-32">
       <div className="text-center mb-9">
@@ -13,26 +26,15 @@ const JobCategory = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 justify-center md:justify-between items-center gap-10 ml-12 md:ml-0">
-        <div className="max-w-xs bg-gradient-to-r from-sky-50 to-blue-100 px-16 py-12 rounded-md">
-          <FontAwesomeIcon className="fa-2x text-blue-500 bg-blue-100 px-5 py-3 rounded-md mb-6" icon={faCalculator} />
-          <h4 className="text-xl font-bold text-gray-800 mb-2">Account & Finance</h4>
-          <p className="text-gray-500">300 Jobs Available</p>
-        </div>
-        <div className="max-w-xs bg-gradient-to-r from-sky-50 to-blue-100 px-16 py-12 rounded-md">
-          <FontAwesomeIcon className="fa-2x text-blue-500 bg-blue-100 px-5 py-3 rounded-md mb-6" icon={faLightbulb} />
-          <h4 className="text-xl font-bold text-gray-800 mb-2">Creative Design</h4>
-          <p className="text-gray-500">100+ Jobs Available</p>
-        </div>
-        <div className="max-w-xs bg-gradient-to-r from-sky-50 to-blue-100 px-16 py-12 rounded-md">
-          <FontAwesomeIcon className="fa-2x text-blue-500 bg-blue-100 px-5 py-3 rounded-md mb-6" icon={faChartSimple} />
-          <h4 className="text-xl font-bold text-gray-800 mb-2">Marketing & Sales</h4>
-          <p className="text-gray-500">150 Jobs Available</p>
-        </div>
-        <div className="max-w-xs bg-gradient-to-r from-sky-50 to-blue-100 px-16 py-12 rounded-md">
-          <FontAwesomeIcon className="fa-2x text-blue-500 bg-blue-100 px-5 py-3 rounded-md mb-6" icon={faMicrochip} />
-          <h4 className="text-xl font-bold text-gray-800 mb-2">Engineering Job</h4>
-          <p className="text-gray-500">224 Jobs Available</p>
-        </div>
+        {categories.map((category) => (
+          <div className="max-w-xs bg-gradient-to-r from-sky-50 to-blue-100 px-16 py-12 rounded-md">
+            <img className="bg-blue-100 px-5 py-3 rounded-md mb-6" src={category.logo} alt="" />
+            <h4 className="text-xl font-bold text-gray-800 mb-2">
+              {category.name}
+            </h4>
+            <p className="text-gray-500">{category.jobs_available} Jobs Available</p>
+          </div>
+        ))}
       </div>
     </div>
   );
